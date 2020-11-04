d46928627734742033c0a10cdf780c75b5be5aba
Aleksanterijaakola <aleksanterijaakola@icloud.com>
2020-11-04 20:54:32 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { AiOutlineGitlab, AiOutlineInstagram } from "react-icons/ai";
 
 function App() {
   return (
     <main>
       <nav>
-        <div>
-          <h1>My portfolio</h1>
-        </div>
+        <article className="tapani">
+          <h1>Hi, I'm Aleksanteri</h1>
+          <h2>Software Engineer.</h2>
+          <h3>CSS / Javascript / React</h3>
+          <button className="gitlab">
+            <AiOutlineGitlab />
+          </button>
+          <button className="ig">
+            <AiOutlineInstagram />
+          </button>
+        </article>
       </nav>
     </main>
   );
